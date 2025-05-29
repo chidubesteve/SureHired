c487690080b9d3a757c51cc63541c0d799d18b45
Chidube Anike <anikechidube@gmail.com>
2025-05-29 21:40:41 +0100

    created a central index file to export all components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import FeaturedJobs from "./FeaturedJobs";
+import Features from "./Features";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export { Header, HeroSection, FeaturedJobs, Features, CTA, Footer };
