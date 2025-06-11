3954a4f81de37e9da5088c5717265b1e8b6c3da0
Chidube Anike <anikechidube@gmail.com>
2025-06-11 20:14:55 +0100

    removed async and promise because id is passed synchronously

--- client/src/app/jobs/[id]/page.tsx
+++ client/src/app/jobs/[id]/page.tsx
@@ -22,8 +22,8 @@ import LocationDisplay from "@/components/showLocationTooltip";
 import { formatPostedDate } from "@/utils/formatDate";
 import { formatSalaryRange } from "@/utils/formatSalaryRange";
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
+const page = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
   // dynamic job id, use to fetch job details
   // static data for now
   const job = Jobs.find((job) => job.id === id);
@@ -85,7 +85,9 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 </div>
                 <div className="flex items-center text-neutral-600">
                   <LuDollarSign className="w-4 h-4 mr-2" />
-                  <span className="text-sm">{formatSalaryRange(job.salary)}</span>
+                  <span className="text-sm">
+                    {formatSalaryRange(job.salary)}
+                  </span>
                 </div>
                 <div className="flex items-center text-neutral-600">
                   <LuUsers className="w-4 h-4 mr-2" />
