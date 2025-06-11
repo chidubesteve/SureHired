b07e0f2a1900051a7a36620eb02b1103367a2b76
Chidube Anike <anikechidube@gmail.com>
2025-06-11 20:01:30 +0100

    constant files for filters

---
+++ client/src/constants/filters.ts
@@ -0,0 +1,31 @@
+export const WORK_STYLES = ["Remote", "Hybrid", "On-site"];
+export const COMPANY_SIZES = ["1-10", "11-50", "51-200", "200-500", "500-1000", "1000+"];
+export const JOB_TYPES = [
+    "Full-time",
+    "Part-time",
+    "Contract",
+    "Internship",
+    "Temporary",
+    "Volunteer",
+];
+export const INDUSTRIES = [
+    "Technology",
+    "Finance",
+    "Healthcare",
+    "Education",
+    "Retail",
+    "Manufacturing",
+    "Hospitality",
+    "Real Estate",
+    "Transportation",
+    "Energy",
+    "Telecommunications",
+    "Entertainment",
+    "Government",
+    "Non-profit",
+    "Construction",
+    "Agriculture",
+    "SaaS",
+    "E-commerce",
+    "Biotechnology",
+];
