0774a1b6f8e39eec29e6e77387b087b146344d06
Chidube Anike <anikechidube@gmail.com>
2025-06-18 05:03:36 +0100

    removed Posted on to avoid duplicated and also clashes when used in other places

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -15,13 +15,13 @@ export function formatPostedDate(postedDate) {
 
   if (daysDifference >= 30) {
     // for jobs older than 30 days (date, 'MMMM d, yyyy')
-    return `Posted on ${format(date, "MMM d, yyyy")}`;
+    return `${format(date, "MMM d, yyyy")}`;
   }
   const seconds = differenceInSeconds(now, date);
   const minutes = differenceInMinutes(now, date);
 
-  if (seconds < 60) return "Posted just now";
-  if (minutes < 1) return "Posted less than a minute ago";
+  if (seconds < 60) return "just now";
+  if (minutes < 1) return "less than a minute ago";
 
   // for jobs within 30 days show relative time (eg: 1 hour ago)
   return `${formatDistanceToNow(date, { addSuffix: true })}`;
