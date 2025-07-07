9853a2af0c834788e77b5edcbc1686cadd3238be
Chidube Anike <anikechidube@gmail.com>
2025-07-08 00:41:14 +0100

    removed excess info in app description

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -8,7 +8,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 export const metadata: Metadata = {
   title: "SureHired | Get hired",
   description:
-    "SureHired is your go-to platform for connecting top talent with exciting career opportunities. Whether you're a job seeker looking for your dream role or an employer seeking the perfect candidate, SureHired delivers a seamless, modern job-board experience. Browse thousands of jobs with powerful search and filter tools, apply with ease, or post openings to attract qualified candidates. Candidates can sign up with LinkedIn, GitHub, or Google, manage their profiles, and track applications, while employers can create detailed job listings and manage applicants through an intuitive dashboard. With a sleek, responsive design and secure checkout powered by Lemon Squeezy (or Stripe) for job postings, SureHired ensures a professional and efficient experience for all users. Find your future, hire with confidence – SureHired!",
+    "SureHired is your go-to platform for connecting top talent with exciting career opportunities. Whether you're a job seeker looking for your dream role or an employer seeking the perfect candidate, SureHired delivers a seamless, modern job-board experience. Find your future, hire with confidence – SureHired!",
   icons: {
     icon: "/favicon.ico", // default
     apple: "/apple-touch-icon.png",
