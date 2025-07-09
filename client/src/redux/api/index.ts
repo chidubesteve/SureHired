c03f2af2b756943bb3d73f68d3465dd8a387758b
Chidube Anike <anikechidube@gmail.com>
2025-07-09 15:28:19 +0100

    query endpoint for get single jobs

--- client/src/redux/api/index.ts
+++ client/src/redux/api/index.ts
@@ -1,24 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { JobResponse } from "../../types/Job";
+import { JobResponse, SingleJobResponse } from "../../types/Job";
 
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+  }),
+  reducerPath: "api",
+  tagTypes: ["Jobs", "Job", "Companies", "Company", "Users", "User"],
+  endpoints: (builder) => ({
+    getFeaturedJobs: builder.query<JobResponse, void>({
+      query: () => "/",
     }),
-    reducerPath: "api",
-    tagTypes: ["Jobs", "Job", "Companies", "Company", "Users", "User"],
-    endpoints: (builder) => ({
-        getFeaturedJobs: builder.query<JobResponse, void>({
-            query:() => "/",
-        }),
-        getAllJobs: builder.query<JobResponse, { page: number, limit: number }>({
-            query: ({ page, limit }) => ({
-                url: "/jobs",
-                params: { page, limit },
-            }),
-        })
+    getAllJobs: builder.query<JobResponse, { page: number; limit: number }>({
+      query: ({ page, limit }) => ({
+        url: "/jobs",
+        params: { page, limit },
+      }),
     }),
+    getJobById: builder.query<SingleJobResponse, { id: string }>({
+      query: ({ id }) => ({
+        url: `/jobs/${id}`,
+      }),
+    }),
+  }),
 });
 
-export const { useGetFeaturedJobsQuery, useGetAllJobsQuery } = api;
+export const { useGetFeaturedJobsQuery, useGetAllJobsQuery, useGetJobByIdQuery } = api;
