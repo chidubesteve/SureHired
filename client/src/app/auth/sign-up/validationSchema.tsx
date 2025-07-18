d3ddc720b230e839f5ae1b87a166aba000adbd4a
Chidube Anike <anikechidube@gmail.com>
2025-07-18 02:02:32 +0100

    added max of 50 characters for first and last name

--- client/src/app/auth/sign-up/validationSchema.tsx
+++ client/src/app/auth/sign-up/validationSchema.tsx
@@ -2,18 +2,18 @@ import { z } from "zod";
 
 export const formSchema = z
   .object({
-    firstName: z.string().min(2, "First name is required"),
-    lastName: z.string().min(2, "Last name is required"),
+    firstName: z.string().min(2, "First name is required").max(50, "First name must be between 2-50 characters"),
+    lastName: z.string().min(2, "Last name is required").max(50, "Last name must be between 2-50 characters"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     company: z.string().optional(),
     agreeToTerms: z.boolean().refine((value) => value === true, {
       message: "You must agree to the terms",
     }),
-    userType: z.enum(["candidate", "employer"]),
+    userType: z.enum(["CANDIDATE", "EMPLOYER"]),
   })
   .superRefine((data, ctx) => {
-    if (data.userType === "employer" && !data.company?.trim()) {
+    if (data.userType === "EMPLOYER" && !data.company?.trim()) {
       ctx.addIssue({
         path: ["company"],
         code: z.ZodIssueCode.custom,
