94ccb8c30a67c6889075719974448a3d0462ff27
Chidube Anike <anikechidube@gmail.com>
2025-07-27 15:50:11 +0100

    wrap sessionprovider around app

---
+++ client/src/app/AuthContext.tsx
@@ -0,0 +1,16 @@
+
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
+export interface AuthContextProps {
+  children: React.ReactNode;
+  session?: Session | null;
+}
+
+export default function AuthContext({ children, session }: AuthContextProps) {
+  return (
+    <SessionProvider session={session } refetchOnWindowFocus>
+      {children}
+    </SessionProvider>
+  );
+}
