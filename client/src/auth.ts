df8cfd59c294d58c2fe412c88dca598077735782
Chidube Anike <anikechidube@gmail.com>
2025-07-27 15:46:59 +0100

    moved auth to root of app

--- client/src/auth.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import LinkedIn from "next-auth/providers/linkedin";
-import Google from "next-auth/providers/google";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [LinkedIn, Google],
-});
