38492be5c312b881a2d867e725cede76f2e23490
Chidube Anike <anikechidube@gmail.com>
2025-08-01 03:19:36 +0100

    added a ma for charter length for names

--- client/src/app/user/profile/validation/ProfileSchema.ts
+++ client/src/app/user/profile/validation/ProfileSchema.ts
@@ -1,8 +1,8 @@
     import { z } from 'zod';
 
     export const ProfileSchema = z.object({
-        firstName: z.string().min(1, 'First name is required'),
-        lastName: z.string().min(1, 'Last name is required'),
+        firstName: z.string().min(1, 'First name is required').max(50, 'First name must be at most 50 characters'),
+        lastName: z.string().min(1, 'Last name is required').max(50, 'Last name must be at most 50 characters'),
     })
 
     export type ProfileSchemaType = z.infer<typeof ProfileSchema>;
