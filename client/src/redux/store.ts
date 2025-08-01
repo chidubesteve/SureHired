e31450acf2a1a029e43a4ffae1993c337914ee88
Chidube Anike <anikechidube@gmail.com>
2025-08-01 03:14:37 +0100

    added auth and  user reducer to store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,16 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { jobsApi } from "./services/Jobs";
 import { companyApi } from "./services/company";
+import { authApi } from "./services/auth";
+import { userApi } from "./services/user";
 
 const store = configureStore({
   reducer: {
     [jobsApi.reducerPath]: jobsApi.reducer,
     [companyApi.reducerPath]: companyApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(jobsApi.middleware)
-      .concat(companyApi.middleware),
+      .concat(companyApi.middleware)
+      .concat(authApi.middleware)
+      .concat(userApi.middleware),
 });
 
 setupListeners(store.dispatch);
