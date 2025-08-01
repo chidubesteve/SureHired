7bcabd78d158d3d4ecc073f37c6524a36a3cf600
Chidube Anike <anikechidube@gmail.com>
2025-08-01 23:07:35 +0100

    changed type to resemble returned data

--- client/src/types/User.ts
+++ client/src/types/User.ts
@@ -1,13 +1,15 @@
 export type UserType = "CANDIDATE" | "EMPLOYER";
 
 export interface PublicUser {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  profilePicture?: string | null;
-  userType: UserType;
-  emailVerified: Date;
-  rememberMe: boolean;
-  companyId?: string; // Optional, only for EMPLOYER type
+  data: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    profilePicture?: string | null;
+    userType: UserType;
+    emailVerified: Date;
+    rememberMe?: boolean;
+    companyId?: string; // Optional, only for EMPLOYER type
+  };
 }
